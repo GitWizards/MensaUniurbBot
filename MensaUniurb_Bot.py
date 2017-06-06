0aaf94baad6a5198bf792ed38c5e2364668af5b0
Radeox <dawid.weglarz95@gmail.com>
2017-06-07 00:35:21 +0200

    Removed logging statistics

--- MensaUniurb_Bot.py
+++ MensaUniurb_Bot.py
@@ -82,7 +82,6 @@ def handle(msg):
 
     if command_input == '/statistiche':
         try:
-            printLog("{0} - {1}".format(chat_id, command_input))
             f = open("log.txt", "r")
 
             # Get current month days
